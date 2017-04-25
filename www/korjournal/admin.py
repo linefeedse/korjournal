4376284c7fc474d2c01bdef3d1ab8f94feed701b
Tor-Ake.Fransson <tor-ake.fransson@linefeed.se>
2017-04-25 14:13:55 +0200

    admin changes

--- www/korjournal/admin.py
+++ www/korjournal/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Vehicle, OdometerSnap
+from .models import Vehicle, OdometerSnap, Invoice, OdometerImage
+
+
+class OdometerImageInline(admin.TabularInline):
+	model = OdometerImage
+
+class OdometerSnapAdmin(admin.ModelAdmin):
+	list_filter = ('driver', )
+	date_hierarchy = 'when'
+	inlines = ( OdometerImageInline, )
+
+class OdometerImageAdmin(admin.ModelAdmin):
+	list_filter = ('driver', )
+
+class InvoiceAdmin(admin.ModelAdmin):
+	list_filter = ('customer', 'is_paid', )
+	date_hierarchy = 'duedate'
 
 admin.site.register(Vehicle)
-admin.site.register(OdometerSnap)
-
+admin.site.register(OdometerSnap, OdometerSnapAdmin)
+admin.site.register(OdometerImage, OdometerImageAdmin)
+admin.site.register(Invoice, InvoiceAdmin)
